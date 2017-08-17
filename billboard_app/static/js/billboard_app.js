f9913a59e40606b9ae407c8f5ab3155e367b2046
Josh <josh.lewittes@gmail.com>
2017-08-17 11:26:15 +0300

    update 3

--- billboard_app/static/js/billboard_app.js
+++ billboard_app/static/js/billboard_app.js
@@ -1,14 +1,31 @@
 $(document).ready(function() {
-        if ($('.messageOne').length) {
-        	console.log('visible')
-            $(".collapse-form").hide()
-        }
+    if ($('.post-wrapper').length) {
+        $(".collapse-form").show();
+    }
+    if ($('.error-container').length) {
+        $(".collapse-form").hide();
+    } else {
+        $(".collapse-form").show();
+    }
 
     $("#add").click(function() {
 		$(".error-container").hide();
+		$(".button-wrapper").hide();
         $(".collapse-form").show();
 	});
 
+	$("#delete").click(function() {
+	 if ($('.post-wrapper').length == 0 ) {
+        $(".collapse-form").show()
+     }
+		$(".button-wrapper").show();
+		$('.error-container').show();
 
+     if ($('.error-container').length) {
+        $(".collapse-form").hide();
+     } else {
+        $(".collapse-form").show();
+     }
 
-    });
+	});
+});
